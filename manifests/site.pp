2664552108146bd583ae1525c06b8634c205a3c1
root <root@shashiudawa4.mylabserver.com>
2017-10-31 05:28:40 +0000

    adding node def site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,7 +12,6 @@
 ## Active Configurations ##
 
 # Disable filebucket by default for all File resources:
-#https://docs.puppet.com/pe/2015.3/release_notes.html#filebucket-resource-no-longer-created-by-default
 File { backup => false }
 
 # DEFAULT NODE
@@ -24,8 +23,14 @@ File { backup => false }
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-
+node  'shashiudawa6.mylabserver.com' {
+  notify { 'test for node def':
+    message => "This is a node definition",
+  }
+}
 node default {
+  #include ntp
+  #class { 'ntp': }
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
